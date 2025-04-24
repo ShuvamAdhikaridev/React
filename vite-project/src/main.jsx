87d3f42038b3edf506661ab237c3ed785ea86dad
Shuvam Adhikari <shuvam.adhikari.dev@gmail.com>
2025-04-24 20:38:05 +0545

    React_Basics

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -2,9 +2,39 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App.jsx";
+import React from "react";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom App</h1>
+    </div>
+  );
+}
+
+// const ReactElement = {
+//   type: "a",
+//   props: {
+//     href: "https://google.com",
+//     target: "_blank",
+//   },
+//   children: "Click me to visit Google",
+// };
+
+const anotherElement = (
+  <a href="https://google.com" target="_blank">
+    Visit Google
+  </a>
 );
+
+const reactElement = React.createElement(
+  "a",
+  {
+    href: "https://google.com",
+    target: "blank",
+  },
+
+  "Click me to Visit Google"
+);
+
+createRoot(document.getElementById("root")).render(<App />);
