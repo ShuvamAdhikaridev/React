106196be49d4355b2d4ead7119ab5c653c5057db
Shuvam Adhikari <shuvam.adhikari.dev@gmail.com>
2025-05-28 14:43:30 +0545

    Changes

--- ReactRouter/src/App.jsx
+++ ReactRouter/src/App.jsx
@@ -1,16 +1,11 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
-
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./components/Home/Home";
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <h1 className="bg-green-600 p-4">React Router</h1>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
--- ReactRouter/src/components/header/Header.jsx
+++ ReactRouter/src/components/header/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Link,NavLink} from react-router-dom;
+import { Link, NavLink } from "react-router-dom";
 
 export default function Header() {
   return (
@@ -7,11 +7,7 @@ export default function Header() {
       <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
           <Link to="/" className="flex items-center">
-            <img
-              src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
-              className="mr-3 h-12"
-              alt="Logo"
-            />
+            <img src="image/logo.jpg" className="mr-3 h-12" alt="Logo" />
           </Link>
           <div className="flex items-center lg:order-2">
             <Link
