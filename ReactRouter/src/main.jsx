929f128b85d82a3e917f53aa34e86431c9283f40
Shuvam Adhikari <shuvam.adhikari.dev@gmail.com>
2025-05-29 14:19:34 +0545

    User:Userid

--- ReactRouter/src/main.jsx
+++ ReactRouter/src/main.jsx
@@ -7,6 +7,7 @@ import Layout from "./Layout";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Contact from "./components/Contactus/Contact";
+import User from "./components/User/User";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -15,6 +16,7 @@ const router = createBrowserRouter([
       { path: "", element: <Home /> },
       { path: "about", element: <About /> },
       { path: "contact", element: <Contact /> },
+      { path: "user/:userid", element: <User /> },
     ],
   },
 ]);
