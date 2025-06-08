5c07679fedf98d217f0a656adf909ed9947c5420
Shuvam Adhikari <shuvam.adhikari.dev@gmail.com>
2025-06-08 22:35:42 +0545

    local-storage

--- context-api-to-do/src/App.jsx
+++ context-api-to-do/src/App.jsx
@@ -1,15 +1,62 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
+import { ToDoProvider } from "./context";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [todos, setToDos] = useState([]);
+
+  const addTodo = (todo) => {
+    setToDos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
+  };
+
+  const updateToDo = (id, todo) => {
+    setToDos((prev) =>
+      prev.map((prevToDo) => (prevToDo.id === id ? todo : prevToDo))
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setToDos((prev) => prev.filter((todo) => todo.id != id));
+  };
+
+  const toggleComplete = (id) => {
+    setToDos((prev) =>
+      prev.map((prevToDo) =>
+        prevToDo === id
+          ? { ...prevToDo, completed: !prevToDo.completed }
+          : prevToDo
+      )
+    );
+  };
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"));
+
+    if (todos && todos.length > 0) {
+      setToDos(todos);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello World</h1>
-    </>
+    <ToDoProvider
+      value={{ todos, addTodo, updateToDo, deleteTodo, toggleComplete }}
+    >
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">
+            Manage Your Todos
+          </h1>
+          <div className="mb-4">{/* Todo form goes here */}</div>
+          <div className="flex flex-wrap gap-y-3">
+            {/*Loop and Add TodoItem here */}
+          </div>
+        </div>
+      </div>
+    </ToDoProvider>
   );
 }
 
